462e22589baad8337af4a54beb39b1b6b5717328
Spacellary <spacellary@gmail.com>
2023-03-24 02:26:35 -0300

    Simple eror handling

--- DalamudBetaKey/Program.cs
+++ DalamudBetaKey/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace ConsoleApp
@@ -16,39 +17,61 @@ namespace ConsoleApp
 
         private static async Task MainAsync(string[] args)
         {
-            if (args is null)
+            try
             {
-                throw new ArgumentNullException(nameof(args));
-            }
+                var client = new HttpClient();
+                var response = await client.GetAsync("https://raw.githubusercontent.com/goatcorp/dalamud-distrib/main/stg/version");
+                var content = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(content);
 
-            var client = new HttpClient();
-            var response = await client.GetAsync("https://raw.githubusercontent.com/goatcorp/dalamud-distrib/main/stg/version");
-            var content = await response.Content.ReadAsStringAsync();
-            var json = JObject.Parse(content);
+                Console.WriteLine("Here's the updated Dalamud Staging Beta Build information:");
+                Console.WriteLine();
 
-            Console.WriteLine("Here's the updated Dalamud Staging Beta Build information:");
-            Console.WriteLine();
-
-            foreach (var property in json.Properties())
-            {
-                Console.WriteLine($"{property.Name}: {property.Value}");
-            }
-
-            Console.WriteLine();
-
-            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher", "dalamudConfig.json");
-
-            Console.Write("Would you like to open the dalamudConfig.json file? (y/N): ");
-            var answer = Console.ReadLine();
-
-            if (answer != null && answer.Equals("y", StringComparison.OrdinalIgnoreCase))
-            {
-                var psi = new ProcessStartInfo
+                foreach (var property in json.Properties())
                 {
-                    FileName = filePath,
-                    UseShellExecute = true
-                };
-                Process.Start(psi);
+                    Console.WriteLine($"{property.Name}: {property.Value}");
+                }
+
+                Console.WriteLine();
+
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher", "dalamudConfig.json");
+
+                string? answer = "";
+                while (answer != "y" && answer != "n")
+                {
+                    Console.Write("Would you like to open the dalamudConfig.json file? (y/N): ");
+                    answer = Console.ReadLine()?.ToLower();
+                    if (string.IsNullOrWhiteSpace(answer))
+                    {
+                        answer = "n";
+                    }
+                }
+
+
+                if (answer == "y")
+                {
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = filePath,
+                        UseShellExecute = true
+                    };
+                    Process.Start(psi);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("There was an error retrieving data from the API:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine("There was an error parsing the JSON data:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error:");
+                Console.WriteLine(ex.Message);
             }
         }
     }
