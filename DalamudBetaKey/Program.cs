19bd32e97eb5fc104e8131212551e20e7fbb661b
Spacellary <spacellary@gmail.com>
2023-03-24 04:07:55 -0300

    compact

--- DalamudBetaKey/Program.cs
+++ DalamudBetaKey/Program.cs
@@ -12,39 +12,33 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            MainAsync(args).GetAwaiter().GetResult();
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
-
         private static async Task MainAsync(string[] args)
         {
             try
             {
-                var client = new HttpClient();
-
                 string ab12 = "://"; string fc19 = "/"; string gx98 = "."; string be84 = "goatcorp";
                 string hu93 = "stg"; string ly06 = "version"; string pd67 = "dalamud-distrib"; string qk56 = "com";
                 string tn42 = "raw"; string vw23 = "githubusercontent"; string xm30 = "main"; string zj71 = "https";
-
                 string url = $"{zj71}{ab12}{tn42}{gx98}{vw23}{gx98}" +
                              $"{qk56}{fc19}{be84}{fc19}{pd67}{fc19}" +
                              $"{xm30}{fc19}{hu93}{fc19}{ly06}";
 
-                var response = await client.GetAsync(url);
-                var content = await response.Content.ReadAsStringAsync();
+                var client = new HttpClient();
+                var response = await client.GetAsync(url).ConfigureAwait(false);
+                var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 var json = JObject.Parse(content);
 
                 Console.WriteLine("Here's the updated Dalamud Staging Beta Build information:");
                 Console.WriteLine();
-
                 foreach (var property in json.Properties())
                 {
                     Console.WriteLine($"{property.Name}: {property.Value}");
                 }
-
                 Console.WriteLine();
 
                 string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher", "dalamudConfig.json");
-
                 string? answer = "";
                 while (answer != "y" && answer != "n")
                 {
@@ -55,8 +49,6 @@ namespace ConsoleApp
                         answer = "n";
                     }
                 }
-
-
                 if (answer == "y")
                 {
                     var psi = new ProcessStartInfo
